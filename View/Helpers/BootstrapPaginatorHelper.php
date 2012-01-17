e221013e4272e722e39e3abe21b5ccafc9a24290
Kim Egede Jakobsen <ke@nodes.dk>
2012-01-17 20:58:38 +0100

    Added table header helper, to make headers sortable (for TwitterBootstrap)

---
+++ View/Helpers/BootstrapPaginatorHelper.php
@@ -0,0 +1,38 @@
+<?php
+App::uses('PaginatorHelper', 'View/Helper');
+/**
+ * Twitter Bootstrap Paginator Helper
+ */
+class BootstrapPaginatorHelper extends PaginatorHelper {
+
+	/**
+	 * Make table headers sortable
+	 *
+	 * @param string $key
+	 * @param mixed $title
+	 * @param array $options
+	 * @return string
+	 */
+	public function sortTableHeader($key, $title = null, $options = array()) {
+		$content = parent::sort($key, $title, $options);
+
+		$options = array_merge(array('url' => array(), 'model' => null), $options);
+
+		$class = "";
+
+		$sortKey = $this->sortKey($options['model']);
+		$defaultModel = $this->defaultModel();
+		$isSorted = (
+			$sortKey === $key ||
+			$sortKey === $defaultModel . '.' . $key ||
+			$key === $defaultModel . '.' . $sortKey
+		);
+
+		if ($isSorted) {
+			$dir = $this->sortDir($options['model']) === 'asc' ? 'desc' : 'asc';
+			$class = $dir === 'asc' ? 'headerSortDown' : 'headerSortUp';
+		}
+
+		return sprintf('<th class="blue header %s">%s</th>', $class, $content);
+	}
+}
